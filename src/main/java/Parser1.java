8042b99067d87f2324d69b384fe1c71d3527c7ef
tavisca-akhatri <akhatri@tavisca.com>
2019-07-26 12:21:33 +0530

    merchant galaxy tdd first commit

--- src/main/java/Parser1.java
+++ src/main/java/Parser1.java
@@ -1,43 +1,14 @@
 public class Parser1
 {
-    public String[] parse(String query)
-    {
-        String split[] = query.split("is ");
-        String split1[] = split[0].split(" ");
-        String split2[] = split[1].split(" ");
-        String[] output = new String[]{split1[0],split1[1],split1[2],split2[0]};
 
-        return output;
+    public String[] parse(String input)
+    {
+          WordToRoman roman = new WordToRoman();
+          String[] output = input.split(" ");
+         // map.put(split[0],split[2]);
+          String[] words = new String[]{output[0],output[2]};
+          roman.StoreInHashMap(words);
+          return words;
     }
 
-    public String[] parse1(String query)
-    {
-        String queryonsplit[] ={};
-        String queryonsplit1[] ={};
-        String output[] = new String[]{};
-        if(query.contains("how much"))
-        {
-            if(query.contains("is"))
-            {
-                queryonsplit = query.split("is ");
-                queryonsplit1 = queryonsplit[1].split(" ");
-                output = new String[]{queryonsplit1[0],queryonsplit1[1],queryonsplit1[2],queryonsplit1[3]};
-                return output;
-            }
-            else
-            {
-                output = new String[]{"I have no idea what you are talking about"};
-                return output;
-            }
-
-        }
-        else if(query.contains("how many"))
-        {
-            queryonsplit = query.split("is ");
-            queryonsplit1 = queryonsplit[1].split(" ");
-            output = new String[]{queryonsplit1[0],queryonsplit1[1],queryonsplit1[2]};
-            return output;
-        }
-        return null;
-    }
 }
---
+++ src/main/java/Parser2.java
@@ -0,0 +1,43 @@
+public class Parser2
+{
+    public String[] parse(String query)
+    {
+        String querysplit[] = query.split("is ");
+        String split1[] = querysplit[0].split(" ");
+        String split2[] = querysplit[1].split(" ");
+        String[] output = new String[]{split1[0],split1[1],split1[2],split2[0]};
+
+        return output;
+    }
+
+    public String[] parse1(String query)
+    {
+        String queryonsplit[] ={};
+        String queryonsplit1[] ={};
+        String output[] = new String[]{};
+        if(query.contains("how much"))
+        {
+            if(query.contains("is"))
+            {
+                queryonsplit = query.split("is ");
+                queryonsplit1 = queryonsplit[1].split(" ");
+                output = new String[]{queryonsplit1[0],queryonsplit1[1],queryonsplit1[2],queryonsplit1[3]};
+                return output;
+            }
+            else
+            {
+                output = new String[]{"I have no idea what you are talking about"};
+                return output;
+            }
+
+        }
+        else if(query.contains("how many"))
+        {
+            queryonsplit = query.split("is ");
+            queryonsplit1 = queryonsplit[1].split(" ");
+            output = new String[]{queryonsplit1[0],queryonsplit1[1],queryonsplit1[2]};
+            return output;
+        }
+        return null;
+    }
+}
